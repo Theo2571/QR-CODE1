192006668939ac9ca90eb1cd5b0589dc336c0349
Matvey <matvey55561@mail.ru>
2022-11-07 16:39:04 +0600

    update

--- src/Pages/PrintPage/Print.js
+++ src/Pages/PrintPage/Print.js
@@ -1,31 +1,52 @@
 import React, {useEffect, useState} from 'react';
 import s from "../MainPage/MainComponents/mainComponents.module.css";
 import DateFilter from "../../components/DataPicker/DataPicker";
-import {getUsers, PrintQr} from "../../store/slices/userSlice";
+import {getUsers, hash, PrintQr} from "../../store/slices/userSlice";
 import Qr from "../../QR/Qr";
 import {useDispatch, useSelector} from "react-redux";
 
 const Print = () => {
-
+    const [arr, setArr] = useState([])
+    const [open , setOpen] = useState(false)
+    const handleOpen = () => setOpen(true);
     const d = new Date()
     const items = useSelector( store => store.userReducer.users);
-
     const dispatch = useDispatch()
     const [date ,setDate] = useState({from: d.setMonth(d.getMonth() - 1), to: new Date()})
 
+
     const print = () => {
         window.print();
-        dispatch(PrintQr(items.map((item) => item.hash)))
-        dispatch(getUsers({limit:9999999,  archived: false}))
+        if (!arr.length) {
+            dispatch(PrintQr(items.map((item) => item.hash)))
+        } else dispatch(PrintQr(arr))
 
+        dispatch(getUsers({limit: 9999999, archived: false}))
     }
-const rep = () => {
-    dispatch(getUsers({limit:9999999,  archived: false}))
+    const rep = () => {
+        dispatch(getUsers({limit:9999999,  archived: false}))
+    }
 
-}
     const norepet = (event) => {
         dispatch(getUsers({print:event.target.value,limit:9999999,  archived: false}))
     }
+
+    const handleHuiClick = ({ hash }) => {
+        const index = arr.indexOf(hash);
+        console.log(arr, "dawddawd")
+        if (index >= 0) {
+            setArr((prev) => {
+                prev.splice(index, 1);
+                return prev;
+            });
+            return;
+        }
+        setArr((prev) => {
+            return [...prev, hash]
+        });
+
+    };
+
     useEffect(() => {
         dispatch(getUsers({limit:9999999,  archived: false}))
     }, []);
@@ -37,32 +58,38 @@ const rep = () => {
                     dispatch( ( getUsers({limit:9999999,  createDateStart: from, createDateEnd: to }) ) );
                 }}/>
 
-                    <div style={{display:"flex", position:"relative", right:150, top:1}}>
+                    <div style={{display:"flex", position:"relative", right:88, top:1}}>
                     <form className={s.pagination} onChange={norepet} >
-                        <select style={{padding:"0 25px"}}   name="city" id="city-select">
-                            <option  value="">-- Фильтр --</option>
-                            <option value="true">Распечатанные</option>
-                            <option value="false">Не распечатанные</option>
+                        <select style={{padding:"0 25px",fontSize:16,}}   name="city" id="city-select">
+                            <option style={{fontSize:16,}} value="">-- Фильтр --</option>
+                            <option style={{fontSize:16,}} value="true">Распечатанные</option>
+                            <option style={{fontSize:16,}} value="false">Не распечатанные</option>
                         </select>
                     </form>
-                    <button className={s.pagination}  onClick={rep}>Сброс</button>
+                    <button style={{ fontSize:16, padding:5, }} className={s.pagination}  onClick={rep}>Сброс</button>
                     </div>
-                    <button style={{position:"relative",top:1}} className={s.bth} onClick={print}>Печать</button>
+                <div style={{display:"flex", flexDirection:"column", gap:12, position:"relative",top:15,}}>
+                <button style={{ fontSize:16, padding:12, }} className={s.bth} onClick={print}>Печать</button>
+                </div>
             </div>
 
 
                 <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
 
-                    <div className={s.active__print} >
+                    <div   className={s.active__print} >
                     {items.map((item) => (
                         <>
-                            <div>
-                                <div style={{margin:10}}>
-                                    <Qr hash={item.hash}/>
+                            <div className={ arr.includes(item.hash) || !arr.length ? `${s.printt} ${s.print__active}` : `${s.printt} `}>
+                                <div onClick={() => handleHuiClick(item)} style={{margin:"30px 20px 0 0"}}>
+                                    <div >
+                                    <Qr hash={item.hash} onClick={(isActive, setIsActive) => {
+                                        setIsActive(!isActive)
+                                    }}/>
                                     <p style={{textAlign:"center", padding:0, margin:0}}>{item.hash}</p>
-                                    <p style={{textAlign:"center", padding:0, margin:0}}>
+                                    <p className={s.checkbox} style={{textAlign:"center", padding:0, margin:0}}>
                                    {item.print ?  <p>Распечатан</p> : <p> Не распечатан</p>}
                                     </p>
+                                    </div>
                                 </div>
                             </div>
                         </>
--- src/QR/Qr.js
+++ src/QR/Qr.js
@@ -1,17 +1,33 @@
 import s from './Qr.module.css'
 import QRCode from "react-qr-code";
-function Qr({hash}) {
-
+import {useState} from "react";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+function Qr({hash, onClick}) {
+    const [isActive, setIsActive] = useState(false);
 const print = () => {
+
     window.print();
 }
+
+
     return (
-        <div>
+        <div onClick={() => onClick(isActive, setIsActive)} >
+            {isActive ?
             <div>
-                <div style={{ display:"flex",padding:10, }}>
+                <div  style={{ display:"flex",padding:10, }}>
                     <QRCode className={s.qr}  value={`http://http://localhost:3000/qr/${hash}`} size={150} />
+                    <div className={s.bth} style={{position:"absolute", margin:"-15px 152px 0 ", color:"#43DC0EFF"}} >
+                      <TaskAltIcon />
+                    </div>
                 </div>
             </div>
+                :
+                <div style={{background: isActive ? '#cccccc' : 'transparent'}}>
+                    <div  style={{ display:"flex",padding:10, }}>
+                        <QRCode className={s.qr}  value={`http://http://localhost:3000/qr/${hash}`} size={150} />
+                    </div>
+                </div>
+            }
             <div style={{textAlign: "center", fontSize: 25 , cursor: "pointer"}}>
                 {/*<button style={{padding: "5px 30px", cursor: "pointer"}} className={s.bth} onClick={print}>Печать</button>*/}
             </div>
